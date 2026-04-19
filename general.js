f67a2c3537562e169a280352d0462b5ec3550425
punit-0522 <punitjaiswal785621@gmail.com>
2026-04-19 12:43:18 +0530

    Enhance logging with emojis in general.js

--- general.js
+++ general.js
@@ -1,13 +1,17 @@
-console.log("GENERAL JS STARTED");
+console.log("🔥 FILE IS RUNNING");
 
 const axios = require('axios');
+
+console.log("🔥 AXIOS LOADED");
 
 const baseURL = 'http://localhost:5000';
 
 const run = async () => {
-    console.log("RUN FUNCTION ENTERED");
+    console.log("🔥 FUNCTION ENTERED");
 
     const res = await axios.get(`${baseURL}/books`);
+
+    console.log("🔥 RESPONSE RECEIVED:");
     console.log(res.data);
 };
 
